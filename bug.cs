ec5ecf6d3ad7dc08f6388cd7274ba9eab3053780
BugHunter-X <askzenai@gmail.com>
2025-01-15 01:42:05 +0100

    Add bug.cs file

---
+++ bug.cs
@@ -0,0 +1,16 @@
+public class ExampleClass
+{
+    private int _myField;
+
+    public int MyProperty
+    {
+        get { return _myField; }
+        set { _myField = value; }
+    }
+
+    public void MyMethod()
+    {
+        // Accessing _myField directly is possible but generally discouraged
+        _myField = 10; 
+    }
+}
